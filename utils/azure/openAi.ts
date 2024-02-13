e349d2b6c8e721c187d3206b8db515bc32c80a03
Adnan Al-farizi Rahmat <adnanarahmat45@gmail.com>
2024-02-13 18:28:57 +0700

    fix: update settings json

--- utils/azure/openAi.ts
+++ utils/azure/openAi.ts
@@ -4,10 +4,10 @@ import { AzureKeyCredential, OpenAIClient } from "@azure/openai"
 const modelName = process.env.AZURE_OPENAI_MODEL_NAME as string
 const searchEndpoint = process.env.AZURE_SEARCH_ENDPOINT as string
 const searchKey = process.env.AZURE_SEARCH_KEY as string
-export const getChatCompletions = async (indexName: string, message: string) => {
+export const getChatCompletions : any = async (indexName: string, message: string) => {
  return await  openAiClient.getChatCompletions(modelName,[{ role: "user", content: message }], {
     azureExtensionOptions: {
-      extensions: [
+      extensions:[
         {
           type: "AzureCognitiveSearch",
           endpoint: searchEndpoint,
