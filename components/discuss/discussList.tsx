ebd4ae27560027c85b32b492ff856b9137ff8847
FajarWG <fajarwahyugumelar@gmail.com>
2024-05-09 00:27:19 +0700

    add: filter discuss

--- components/discuss/discussList.tsx
+++ components/discuss/discussList.tsx
@@ -3,11 +3,16 @@ import React from "react";
 import { Card, CardBody } from "@nextui-org/card";
 
 import DiscussItem from "./discussItem";
+import FilterDiscuss from "./filterDiscuss";
 
 export default function DiscussList() {
   return (
     <Card className="lg:max-w-[811px] w-full">
       <CardBody className="flex flex-col gap-5 items-center">
+        <FilterDiscuss />
+        <DiscussItem />
+        <DiscussItem />
+        <DiscussItem />
         <DiscussItem />
       </CardBody>
     </Card>
