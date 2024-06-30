3ceb50a68a3a93eba6ad877f6f36a14b9234a06b
Lutfian Rahdiansyah <lutfian.rhdn@gmail.com>
2024-06-30 18:32:40 +0700

    Feat/review quiz user (#7)
    * feat:review quiz :sparkles:
    
    * feat: add start at on quiz created :sparkles:

--- app/api/quiz/[id]/route.ts
+++ app/api/quiz/[id]/route.ts
@@ -23,25 +23,34 @@ export async function POST(req: Request, { params }: any) {
   }) ;
   if (!quiz) return getResponse(null, 'quiz not found', 400);
   const answerKeys: Array<any> | any = quiz.answer;
-  const question = quiz?.question as [] || []
-  const questionLength :number =question.length
-  let countTrueAnswer = 0;
-  answerKeys.forEach((answerKey: Answer,index:number) => {
-    const answer = answers.find((answer: Answer) => answer.title === answerKey.title)
-    if (answer?.answer.sort().toString() === answerKey.answer.sort().toString()) countTrueAnswer++
-  });
-  const score = countTrueAnswer / questionLength * 100
-  console.log(score)
-
-  const quizResult = await prisma.user_quiz.create({
-    data: {      
-      quiz_id: +id,
-      user_id: session?.id as number ||1,
-      answer: answers as any,
-      duration,
-      score
+  const question :any = quiz?.question as [] || []
+  let totalPoints =0
+ 
+  
+  const answerMapped = answerKeys.map((answerKey: Answer, index: number) => {
+    let answer = answers.find(
+		(answer: Answer) => answer.title === answerKey.title
+    );
+       if (!answer) {
+			answer = {title:question[index].title,answer:[],points:0};
+		}
+    if (answer?.answer.sort().toString() === answerKey.answer.sort().toString()) {
+      totalPoints += +question[index].points 
+      answer.points = question[index].points;
     }
+ 
+return answer
+    
   })
+  const quizResult = await prisma.user_quiz.create({
+		data: {
+			quiz_id: +id,
+			user_id: (session?.id as number) || 1,
+			answer: answerMapped as any,
+			duration,
+			score: totalPoints,
+		},
+  });
 
   return getResponse(quizResult,"success answered a quiz",200)
 }
