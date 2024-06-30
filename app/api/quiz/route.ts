e2f68da90ccab88f0476f5993a4bdd2c1c457218
lutfianRhdn <lutfian.rhdn@gmail.com>
2024-06-30 18:19:00 +0700

    feat:review quiz :sparkles:

--- app/api/quiz/route.ts
+++ app/api/quiz/route.ts
@@ -7,14 +7,29 @@ const prisma = new PrismaClient()
 export async function POST(req: Request) {
   const { course_id, name, question, answer, type, deadline, duration }: Quiz =
 		await req.json();
-  const session = await getSessionUser();
   if (!course_id || !name || !question || !answer) return getResponse(null, 'course_id,name,question,answer is required', 400);
   if (question.length !== answer.length) return getResponse(null, 'question and answer must be same length', 400);
+  const isHaveValidQuestionKeys =
+		question.filter(
+			(item:any) =>
+				!(
+					item.hasOwnProperty("title") &&
+					item.hasOwnProperty("choices") &&
+					item.hasOwnProperty("points")
+				)
+    ).length > 0;
+  if (isHaveValidQuestionKeys) return getResponse(null, 'question must have title, choices, points', 400);
+  const totalPoints = question.reduce(
+    (acc: any, item: any) => +acc + +item.points,
+    0
+  );
+  if (totalPoints !== 100) return getResponse(null, 'total points must be 100', 400);
+  
   const course = await prisma.course.findUnique({
     where: {
-      id: +course_id
-    }
-  })
+      id: +course_id,
+    },
+  });
   if(!course) return getResponse(null, 'course not found', 400);
   const quiz = await prisma.quiz.create({
 		data: {
