c3268fd1bd77639f830572c79a7a43e88c5a7c70
FajarWG <fajarwahyugumelar@gmail.com>
2024-07-03 08:41:36 +0700

    feat: page settings

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -57,7 +57,7 @@ function Sidebar({ open, toggle }: any) {
   return (
     <aside
       ref={sidebarRef}
-      className={`absolute min-h-full top-0 w-fit z-50 bg-white dark:bg-black shadow-lg transition-all ease-in-out duration-300 px-4 py-4 flex flex-col gap-10 ${
+      className={`absolute min-h-full w-[280px] top-0 z-50 bg-white dark:bg-black shadow-lg transition-all ease-in-out duration-300 px-4 py-4 flex flex-col gap-10 ${
         open ? "translate-x-0" : "-translate-x-full"
       }`}
     >
@@ -78,21 +78,42 @@ function Sidebar({ open, toggle }: any) {
             <NextLink
               className={`${
                 isActive ? "bg-dark-blue text-white" : "text-gray-400"
-              } flex justify-between items-center w-44 hover:bg-blue-200 hover:text-blue-800 rounded-md p-3 md:px-5 gap-3`}
+              } flex justify-between items-center w-full hover:bg-blue-200 hover:text-blue-800 rounded-xl p-3 md:px-5 gap-3 h-14`}
               href={item.href}
               key={index}
               onClick={open ? closeSidebarHandler : undefined}
             >
-              <div className="flex items-center gap-2">
+              <div className="flex items-center gap-6">
                 <FontAwesomeIcon icon={item.icon} size="sm" />
 
                 <span
-                  className={` md:overflow-hidden transition-all text-xs md:text-sm text-start font-medium`}
+                  className={` md:overflow-hidden transition-all text-md text-start font-semibold`}
                 >
                   {item.label}
                 </span>
               </div>
-              <FontAwesomeIcon icon={faCircleChevronRight} size="sm" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="21"
+                height="21"
+                viewBox="0 0 21 21"
+                fill="none"
+              >
+                <path
+                  d="M7.74349 18.5146H12.7435C16.9102 18.5146 18.5768 16.8479 18.5768 12.6812V7.68123C18.5768 3.51457 16.9102 1.8479 12.7435 1.8479H7.74349C3.57682 1.8479 1.91016 3.51457 1.91016 7.68123V12.6812C1.91016 16.8479 3.57682 18.5146 7.74349 18.5146Z"
+                  stroke={isActive ? "white" : "gray"}
+                  strokeWidth="1.25"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M9.19385 13.1228L12.1272 10.1812L9.19385 7.2395"
+                  stroke={isActive ? "white" : "gray"}
+                  strokeWidth="1.25"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
             </NextLink>
           );
         })}
--- layouts/AdminLayout.tsx
+++ layouts/AdminLayout.tsx
@@ -1,38 +1,42 @@
-import React from 'react';
-import Sidebar from '@/components/sidebar';
-import { faBars, faBookOpenReader, faChalkboardUser, faUsers } from '@fortawesome/free-solid-svg-icons';
-import { ThemeSwitch } from '@/components/theme-switch';
-import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Table, Tab } from "@nextui-org/react";
-import Layout from './layout';
+import React from "react";
+import { Card, CardHeader, CardBody, Divider } from "@nextui-org/react";
+import Layout from "./layout";
 
 const AdminLayout = ({
+  header,
   title,
   subtitle,
   children,
+  footer,
 }: {
-    children: React.ReactNode;
-    title: string;
-    subtitle: string;
+  header?: React.ReactNode;
+  children: React.ReactNode;
+  title: string;
+  subtitle: string;
+  footer?: React.ReactNode;
 }) => {
-  
   return (
     <Layout>
-      <section className='py-5 my-5'>
-        <div className='container mx-auto w-screen md:w-full max-w-7xl mt-10 px-4'>
+      <section className="py-5 my-5">
+        <div
+          className={`container mx-auto w-screen md:w-full max-w-7xl px-4  ${
+            header ? "" : "mt-10"
+          }`}
+        >
+          {header}
           <Card className="p-2 w-full">
             <CardHeader className="flex gap-3">
               <div className="flex flex-col">
                 <p className="text-2xl font-bold">{title || "TItle"}</p>
-                <p className="text-sm">{ subtitle ||'subTitle'}</p>
+                <p className="text-sm">{subtitle || "subTitle"}</p>
               </div>
             </CardHeader>
             <Divider />
-            <CardBody className="h-[75vh]" >
-              {children}
-            </CardBody>
+            <CardBody className="h-[75vh]">{children}</CardBody>
           </Card>
         </div>
       </section>
+      {footer}
     </Layout>
   );
 };
