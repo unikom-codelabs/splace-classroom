a26eee2f3208c61dfa938b6cf64fc7175ef74af3
FajarWG <fajarwahyugumelar@gmail.com>
2024-07-26 06:23:59 +0700

    add: loading skeleton discuss

--- components/discuss/discussList.tsx
+++ components/discuss/discussList.tsx
@@ -4,7 +4,8 @@ import { Card, CardBody } from "@nextui-org/card";
 
 import DiscussItem from "./discussItem";
 import FilterDiscuss from "./filter/filterDiscuss";
-import Loading from "@/app/course/[id]/loading";
+
+import { Skeleton } from "@nextui-org/react";
 
 export default function DiscussList({ data, isLoading, isBookmark }: any) {
   return (
@@ -19,7 +20,26 @@ export default function DiscussList({ data, isLoading, isBookmark }: any) {
             />
           ))
         ) : (
-          <Loading />
+          <CardBody className="flex flex-col gap-5 p-4">
+            <div className="flex items-center content-between justify-between">
+              <div className="flex gap-2 items-center justify-center">
+                <div>
+                  <Skeleton className="flex rounded-full w-9 h-9" />
+                </div>
+
+                <Skeleton className="h-3 w-3/5 rounded-lg " />
+              </div>
+            </div>
+            <div className="gap-2 grid grid-cols-3 ">
+              <div>
+                <Skeleton className="flex rounded-md w-[200px] h-[100px]" />
+              </div>
+            </div>
+
+            <Skeleton className="h-3 w-3/5 rounded-lg " />
+            <Skeleton className="h-3 w-3/6 rounded-lg " />
+            <Skeleton className="h-3 w-3/12 rounded-lg " />
+          </CardBody>
         )}
       </CardBody>
     </Card>
--- components/discuss/userCard.tsx
+++ components/discuss/userCard.tsx
@@ -1,13 +1,29 @@
 "use client";
 import { Button } from "@nextui-org/button";
 import { Card, CardBody } from "@nextui-org/card";
-import { Avatar, AvatarIcon, Divider, useDisclosure } from "@nextui-org/react";
-import React from "react";
+import {
+  Avatar,
+  AvatarIcon,
+  Divider,
+  Skeleton,
+  useDisclosure,
+} from "@nextui-org/react";
+import React, { useState, useEffect } from "react";
 import AddDiscuss from "./addDiscuss/addDiscuss";
 import Loading from "@/app/course/[id]/loading";
+import fetchApi from "@/utils/fetchApi";
 
 export default function CardUser({ data }: any) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const [totalDiscussion, setTotalDiscussion] = useState(0);
+  useEffect(() => {
+    if (data) {
+      fetchApi(`/discustions?user_id=${data.user.id}`, "GET").then((res) => {
+        setTotalDiscussion(res.data.length);
+      });
+    }
+  }, [data]);
 
   return (
     <>
@@ -27,11 +43,21 @@ export default function CardUser({ data }: any) {
               <Divider />
               <div className="flex flex-row gap-5 items-center">
                 <p className="text-sm">Total Discussion</p>
-                <p className=" text-base text-dark-blue">11</p>
+                <p className=" text-base text-dark-blue">{totalDiscussion}</p>
               </div>
             </CardBody>
           ) : (
-            <Loading />
+            <div className="max-w-[300px] w-full flex flex-col justify-center items-center gap-3 p-3">
+              <div>
+                <Skeleton className="flex rounded-full w-14 h-14" />
+              </div>
+              <div className="w-full flex flex-col justify-center items-center gap-2">
+                <Skeleton className="h-3 w-3/5 rounded-lg my-6" />
+                <Divider />
+
+                <Skeleton className="h-3 w-4/5 rounded-lg mt-4" />
+              </div>
+            </div>
           )}
         </Card>
         <Button
